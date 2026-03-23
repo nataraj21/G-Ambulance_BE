74ce3197b5c9403e5efffb1744ca2675e44d7085
Ashwin-DTG <ashwin@datatechgenius.com>
2026-03-23 14:51:44 +0530

    patch fix

--- Controllers/LocationController.cs
+++ Controllers/LocationController.cs
@@ -140,5 +140,22 @@ namespace AmbulanceAPI.Controllers
 
             return location;
         }
+
+        private double GetDistance(double lat1, double lon1, double lat2, double lon2)
+        {
+            var r = 6371.0; // Radius of the Earth in km
+            var dLat = ToRadians(lat2 - lat1);
+            var dLon = ToRadians(lon2 - lon1);
+            var a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                    Math.Cos(ToRadians(lat1)) * Math.Cos(ToRadians(lat2)) *
+                    Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+            var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            return r * c;
+        }
+
+        private double ToRadians(double angle)
+        {
+            return Math.PI * angle / 180.0;
+        }
     }
 }
