83c5938950866b80bf61c99c300c76e0ba469e78
Ashwin-DTG <ashwin@datatechgenius.com>
2026-03-23 14:51:21 +0530

    patch-fix

---
+++ Models/Officer.cs
@@ -0,0 +1,11 @@
+﻿namespace AmbulanceAPI.Models
+{
+    public class Officer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public ICollection<OfficerLocation> Locations { get; set; }
+    }
+}
